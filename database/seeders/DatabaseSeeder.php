0885a08164420bec1d2f556b9d6d44cab16005ca
falgunioctacore <falgunioctacore@gmail.com>
2026-06-01 14:02:01 +0530

    seeder comment remove

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,8 +23,8 @@ class DatabaseSeeder extends Seeder
 
 
         $this->call([
-        // RolesAndPermissionsSeeder::class,
-        // PlanSeeder::class,
+         RolesAndPermissionsSeeder::class,
+         PlanSeeder::class,
          SuperAdminSeeder::class,
          MessageTemplateSeeder::class,
          NotificationSeeder::class,
