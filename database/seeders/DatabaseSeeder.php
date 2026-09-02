c858dc84e8c4745996706c7b60964cd382625f9e
falgunioctacore <falgunioctacore@gmail.com>
2026-09-02 09:24:38 +0530

    uncomment planseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
          RolesAndPermissionsSeeder::class,
-        //  PlanSeeder::class,
+         PlanSeeder::class,
          SuperAdminSeeder::class,
          MessageTemplateSeeder::class,
          NotificationSeeder::class,
